3c4b03a8b0dc70ee1e46940541964931a2c295c4
Maxwellwr <mmaslich@gmail.com>
2018-02-11 17:02:20 +0300

    Fix

--- res/web/CWPIO/CWPIO/Startup.cs
+++ res/web/CWPIO/CWPIO/Startup.cs
@@ -59,6 +59,30 @@ namespace CWPIO
                 .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
                 .AddDataAnnotationsLocalization();
 
+            services.Configure<IdentityOptions>(options =>
+             {
+                 // Password settings
+                 options.Password.RequireDigit = true;
+                 options.Password.RequiredLength = 8;
+                 options.Password.RequireNonAlphanumeric = false;
+                 options.Password.RequireUppercase = true;
+                 options.Password.RequireLowercase = true;
+                 options.Password.RequiredUniqueChars = 6;
+
+                 // Lockout settings
+                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                 options.Lockout.MaxFailedAccessAttempts = 5;
+                 options.Lockout.AllowedForNewUsers = true;
+
+                 // User settings
+                 options.User.RequireUniqueEmail = true;
+
+                 // Confirmations
+                 options.SignIn.RequireConfirmedEmail = false;
+                 options.SignIn.RequireConfirmedPhoneNumber = false;
+             });
+
+
             services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
             services.Configure<SlackSettings>(Configuration.GetSection("SlackWebHooks"));
 
