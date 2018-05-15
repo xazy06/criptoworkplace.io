1ad069128772650eb14fa495443d36bf23db9e5b
Maxwellwr <mmaslich@gmail.com>
2018-05-15 13:47:00 +0300

    rename

--- res/Etherium/CWP/contracts/CWTPTokenSale.sol
+++ res/Etherium/CWP/contracts/CWTPTokenSale.sol
@@ -5,25 +5,24 @@ import "./TokenAddr.sol";
 import "zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol";
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract CWPPTokenSale is TokenAddr, CappedCrowdsale, Ownable {
+contract CWTPTokenSale is TokenAddr, CappedCrowdsale, Ownable {
     /**
-   * CWPTokenSale - Contract for CryptoWorkPlace Token Sale
+   * CWTPTokenSale - Contract for CryptoWorkPlace Token Pre-Sale
    *
    * @param _startTime UNIX Timestamp in seconds, when Token Sale start
    * @param _endTime  Token Sale end time in seconds
    * @param _wallet    Address of multisig wallet, using for refund if Token Sale failed
    * @param  _tokenAddress  Address of Pre-sale CWPToken contract, deployed to blockchain
    */
-  function CWPPTokenSale(uint256 _startTime, uint256 _endTime, address _wallet, address _tokenAddress) public
+  function CWTPTokenSale(uint256 _startTime, uint256 _endTime, address _wallet, address _tokenAddress) public
     TokenAddr(_tokenAddress)
     CappedCrowdsale(1351 * 1 ether)
     Crowdsale(_startTime, _endTime, 1000, _wallet)
   {
   }
 
-  /*Use CWPToken for sale*/
+  /*Use CWTPToken for sale*/
   function createTokenContract() internal returns (MintableToken) {
-    //return new MintableToken();
     return CWTPToken(tokenAddress);
   }
 
