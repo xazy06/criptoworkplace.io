a8b92ac89ff3667fc1692fd798ee1aee9087a491
Maxwellwr <mmaslich@gmail.com>
2018-06-03 20:54:01 +0300

    f

--- res/web/CWPIO/CWPIO/Areas/v1/Models/SimpleApplicationUserSettingsDto.cs
+++ res/web/CWPIO/CWPIO/Areas/v1/Models/SimpleApplicationUserSettingsDto.cs
@@ -8,5 +8,9 @@ namespace CWPIO.Areas.v1.Models
     public class SimpleApplicationUserSettingsDto
     {
         public string EthAddress { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string TelegramNickname { get; set; }
     }
 }
