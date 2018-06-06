14dd212f63827c107e1aa591e0988b118f0ee2e7
Arthur Enokyan <xazy06@gmail.com>
2018-06-06 16:22:34 +0300

    lk metamask initialization

--- res/web/CWPIO/CWPIO/wwwroot/js/controllers/exchanger/index.js
+++ res/web/CWPIO/CWPIO/wwwroot/js/controllers/exchanger/index.js
@@ -81,9 +81,31 @@ var Controller = function () {
 			});
 		}
 	};
+
+	this.web3js = null;
+	
+	this.initWeb3Js = function () {
+				
+		(function () {
+
+			// Checking if Web3 has been injected by the browser (Mist/MetaMask)
+			if (typeof web3 !== 'undefined') {
+				// Use Mist/MetaMask's provider
+				self.web3js = new Web3(web3.currentProvider);
+			} else {
+				console.log('No web3? You should consider trying MetaMask!');
+				// fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+				self.web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+			}
+		})();
+	};
 	
 	this.init = function () {
 		ko.applyBindings(ViewModel);
+
+		self.actions.initWeb3Js();
+		
+		self.actions.usersettings();
 		
 		return this;
 	};
