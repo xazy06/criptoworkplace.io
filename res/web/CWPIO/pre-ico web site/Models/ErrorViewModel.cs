a30efc3ea09c7dcbbf11537608fced81517e6707
Arthur Enokyan <xazy06@gmail.com>
2018-09-19 19:54:37 +0300

    pre-ico web site refactoring

---
+++ res/web/CWPIO/pre-ico web site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pre_ico_web_site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
