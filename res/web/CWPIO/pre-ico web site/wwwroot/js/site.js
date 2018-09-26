ed7137f1c7d98df6818816721296df0ed6fc4038
Arthur Enokyan <xazy06@gmail.com>
2018-09-26 04:14:47 +0300

    pre-ico web site

--- res/web/CWPIO/pre-ico web site/wwwroot/js/site.js
+++ res/web/CWPIO/pre-ico web site/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿// Write your JavaScript code.
+﻿var app = (function () {
+	var App = new function () {
+		this.public = {};
+	}();
+	
+	App.init = function () {
+		i18n.init({lng: "en"}, function(err, t) {
+			// translate nav
+			$("body").i18n();
+		});
+	};
+	
+	App.init();
+	
+	return App.public;
+	
+})();
