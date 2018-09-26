48135d2efd548389413b99ac572811b4445c4d1a
Arthur Enokyan <xazy06@gmail.com>
2018-09-26 21:01:41 +0300

    pre-ico web site

--- res/web/CWPIO/pre-ico web site/wwwroot/js/site.js
+++ res/web/CWPIO/pre-ico web site/wwwroot/js/site.js
@@ -1,13 +1,32 @@
 ﻿var app = (function () {
 	var App = new function () {
-		this.public = {};
+
+		var self = this;
+		
+		this.initTimer = function () {
+			var timer = new Timer();
+			timer.start({countdown: true, startValues: {days: 4, hours:2, minutes:59}});
+
+			try{
+				$('.js-day').text(timer.getTimeValues().days);
+				$('.js-hour').text(timer.getTimeValues().hours);
+				$('.js-min').text(timer.getTimeValues().minutes);
+			}catch(e){console.log(e)}
+		};
+		
+		this.public = {
+			initTimer: self.initTimer
+		};
+		
 	}();
 	
 	App.init = function () {
 		i18n.init({lng: "en"}, function(err, t) {
-			// translate nav
+			
 			$("body").i18n();
 		});
+		
+		App.initTimer();
 	};
 	
 	App.init();
