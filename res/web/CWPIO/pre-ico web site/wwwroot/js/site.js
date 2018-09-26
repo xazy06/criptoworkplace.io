bb2725ad0bf6d6e0b847d977eccee708a8792f1a
Arthur Enokyan <xazy06@gmail.com>
2018-09-26 21:14:17 +0300

    pre-ico web site

--- res/web/CWPIO/pre-ico web site/wwwroot/js/site.js
+++ res/web/CWPIO/pre-ico web site/wwwroot/js/site.js
@@ -4,14 +4,24 @@
 		var self = this;
 		
 		this.initTimer = function () {
-			var timer = new Timer();
-			timer.start({countdown: true, startValues: {days: 4, hours:2, minutes:59}});
+			var timer = new Timer(), updater,
+				a = new Date();
+			timer.start({countdown: true, startValues: {days: (30 - a.getDate()), hours:(23 - a.getHours()), minutes:(60 - a.getMinutes())}});
 
-			try{
-				$('.js-day').text(timer.getTimeValues().days);
-				$('.js-hour').text(timer.getTimeValues().hours);
-				$('.js-min').text(timer.getTimeValues().minutes);
-			}catch(e){console.log(e)}
+			updater = function () {
+				try{
+					$('.js-day').text(timer.getTimeValues().days);
+					$('.js-hour').text(timer.getTimeValues().hours);
+					$('.js-min').text(timer.getTimeValues().minutes);
+				}catch(e){console.log(e)}	
+			};
+
+			updater();
+
+			$(timer).on('minutesUpdated', function (e) {
+				//console.log('update');
+				updater();
+			});
 		};
 		
 		this.public = {
