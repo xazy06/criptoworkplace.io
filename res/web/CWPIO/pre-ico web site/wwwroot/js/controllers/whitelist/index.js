0c4c9db93c22c23b62a9cfdba8cc218e47a2e468
Arthur Enokyan <xazy06@gmail.com>
2018-10-02 02:06:58 +0300

    https://dev.azure.com/unidefence/ico/_workitems/edit/25

--- res/web/CWPIO/pre-ico web site/wwwroot/js/controllers/whitelist/index.js
+++ res/web/CWPIO/pre-ico web site/wwwroot/js/controllers/whitelist/index.js
@@ -19,8 +19,32 @@ var Controller = function () {
 	};
 
 	this.web3js = null;
+
+	this.initWeb3Js = function () {
+
+		(function () {
+
+			// Checking if Web3 has been injected by the browser (Mist/MetaMask)
+			if (typeof web3 !== 'undefined') {
+				// Use Mist/MetaMask's provider
+				self.web3js = new Web3(web3.currentProvider);
+				console.log('web3 js success inited')
+			} else {
+				console.log('No web3? You should consider trying MetaMask!');
+				// fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+				self.web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+			}
+			
+			try{
+				ViewModel.obs.hasMetamask(web3.currentProvider.isMetaMask === true);
+			}catch (e){
+			}
+		})();
+	};
 	
 	this.init = function () {
+		self.initWeb3Js();
+		
 		ko.applyBindings(ViewModel);
 		
 		return this;
@@ -29,17 +53,29 @@ var Controller = function () {
 	
 	var ViewModel = {
 		obs:{
-			ercAddress:ko.observable('')
+			ercAddress:ko.observable(''),
+			hasMetamask: ko.observable(false)
 		},
 		
 		flags:{
 			continueEnabled:ko.pureComputed(function () {
 				return ViewModel.obs.ercAddress().length === 42 && ViewModel.actions.byteLength(ViewModel.obs.ercAddress()) > 20;
 			}),
-			checking: ko.observable(false)
+			checking: ko.observable(false),
+			lockFill: ko.observable(false)
 		},
 		
 		actions:{
+			getndFillAddressFromMetamask: function () {
+				ViewModel.flags.lockFill(true);
+				self.web3js.eth.getAccounts().then(function(r){
+					ViewModel.flags.lockFill(false);
+					try{
+						r = r && r[0] || '';
+						ViewModel.obs.ercAddress(r);
+					}catch (e){}
+				})
+			},
 			byteLength: function (str) {
 				var s = str.length;
 				for (var i=str.length-1; i>=0; i--) {
