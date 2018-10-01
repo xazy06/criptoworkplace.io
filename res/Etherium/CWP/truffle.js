eb6f5b51db5adb9b0225fe3896ad3000d9409c33
Maxwellwr <mmaslich@gmail.com>
2018-10-01 14:49:14 +0300

    refund

--- res/Etherium/CWP/truffle.js
+++ res/Etherium/CWP/truffle.js
@@ -1,6 +1,7 @@
  var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
 // var infura_apikey = "roht23j583p4SPym7gx6";
- const privateKeys = "18cebf64a44af8621c7664974be1d7d30bc943f2b69533c422d8868f59d294c2"; 
+ //const privateKeys = "18cebf64a44af8621c7664974be1d7d30bc943f2b69533c422d8868f59d294c2"; 
+ const privateKeys = "07eb1aab3c62d6e1fce1343e6dc946e7a90c521846364e0934651a67d832a8d8"; 
 
 
 module.exports = {
@@ -22,12 +23,13 @@ module.exports = {
       gasPrice:20000000000
     },
     main: {
-        host: "192.168.68.4",
-        port: 8545,
+        // host: "192.168.68.4",
+        // port: 8545,
+        provider: new HDWalletProvider(privateKeys, "https://ethnode.cryptoworkplace.io"),
         network_id: 1,
-        from: "0x00406eDfc8e186E2FFF7394f1a6e3796DCa4e59f",
+        from: "0x00406edfc8e186e2fff7394f1a6e3796dca4e59f",
         gas: 4612388,
-        gasPrice:20000000000
+        gasPrice:8000000000
     }
   }
 };
