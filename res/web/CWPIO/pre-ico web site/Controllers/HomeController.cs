6198c9ae20ef09c9aa3a7e44a73b665364df23c9
Maxwellwr <mmaslich@gmail.com>
2018-10-02 23:30:41 +0300

    add model to progress bar

--- res/web/CWPIO/pre-ico web site/Controllers/HomeController.cs
+++ res/web/CWPIO/pre-ico web site/Controllers/HomeController.cs
@@ -4,15 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Nethereum.Util;
+using pre_ico_web_site.Eth;
 using pre_ico_web_site.Models;
 
 namespace pre_ico_web_site.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly TokenSaleContract _contract;
+        public HomeController(TokenSaleContract contract)
         {
-            return View();
+            _contract = contract;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new BarModel
+            {
+                Sold = (int)UnitConversion.Convert.FromWei(await _contract.TokenSoldAsync()),
+                Cap = (int)UnitConversion.Convert.FromWei(await _contract.GetCapAsync())
+            };
+            return View(model);
         }
 
         public IActionResult About()
---
+++ res/web/CWPIO/pre-ico web site/Models/BarModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pre_ico_web_site.Models
+{
+    public class BarModel
+    {
+        public int Sold { get; set; }
+        public int Cap { get; set; }
+        public float Percent => Sold * 100 / Cap;
+    }
+}
