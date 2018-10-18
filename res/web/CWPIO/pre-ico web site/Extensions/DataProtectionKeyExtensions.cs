22076409ab71e1f2d627abf1702f532d10d4a5da
Maxwellwr <mmaslich@gmail.com>
2018-10-18 17:01:56 +0300

    add logging

--- res/web/CWPIO/pre-ico web site/Extensions/DataProtectionKeyExtensions.cs
+++ res/web/CWPIO/pre-ico web site/Extensions/DataProtectionKeyExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using pre_ico_web_site.Data;
 using System;
@@ -17,7 +18,7 @@ namespace Microsoft.AspNetCore.DataProtection
 
             builder.Services.AddSingleton<IConfigureOptions<KeyManagementOptions>>(services =>
             {
-                var dbContext = services.GetService<ApplicationDbContext>() ?? new ApplicationDbContext(new EntityFrameworkCore.DbContextOptions<ApplicationDbContext>());
+                var dbContext = services.GetService<ApplicationDbContext>() ?? new ApplicationDbContext(new EntityFrameworkCore.DbContextOptions<ApplicationDbContext>(), services.GetService<ILogger<ApplicationDbContext>>());
                 return new ConfigureOptions<KeyManagementOptions>(options =>
                 {
                     options.XmlRepository = new DataProtectionKeyRepository(dbContext);
