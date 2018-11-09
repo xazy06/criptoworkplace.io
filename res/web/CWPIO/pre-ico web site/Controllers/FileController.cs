2b6690b339998999edff24e6eb076c2361d9e86b
Maxwellwr <mmaslich@gmail.com>
2018-11-09 13:57:36 +0300

    https://dev.azure.com/unidefence/ico/_workitems/edit/149

--- res/web/CWPIO/pre-ico web site/Controllers/FileController.cs
+++ res/web/CWPIO/pre-ico web site/Controllers/FileController.cs
@@ -17,22 +17,30 @@ namespace pre_ico_web_site.Controllers
         [HttpGet("{fileName}")]
         public async Task<IActionResult> Download([FromRoute]string fileName)
         {
-            var stream = new MemoryStream();
+            try
+            {
+                var stream = new MemoryStream();
 
-            var mime = await _files.GetFileByNameAsync(fileName, stream);
+                var mime = await _files.GetFileByNameAsync(fileName, stream);
 
-            if (stream == null || stream.Length == 0)
+                if (stream == null || stream.Length == 0)
+                {
+                    return NotFound();
+                }
+                stream.Seek(0, SeekOrigin.Begin);
+                System.Net.Mime.ContentDisposition cd = new System.Net.Mime.ContentDisposition
+                {
+                    FileName = fileName,
+                    Inline = true
+                };
+                Response.Headers.Add("Content-Disposition", cd.ToString());
+                return File(stream, mime);
+            }
+            catch (FileNotFoundException)
             {
                 return NotFound();
             }
-            stream.Seek(0, SeekOrigin.Begin);
-            System.Net.Mime.ContentDisposition cd = new System.Net.Mime.ContentDisposition
-            {
-                FileName = fileName,
-                Inline = true
-            };
-            Response.Headers.Add("Content-Disposition", cd.ToString());
-            return File(stream, mime);
+
         }
     }
 }
